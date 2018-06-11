c961505d16b0042649ea54c517e90c2f7f966b10
SpaceEEC <spaceeec@yahoo.com>
2018-06-11 22:09:57 +0200

    fix(Collection): mark first(Key), last(Key), and random(Key) as possibly undefined

--- index.d.ts
+++ index.d.ts
@@ -303,20 +303,20 @@ declare module 'discord.js' {
 		public filter(fn: (value: V, key: K, collection: Collection<K, V>) => boolean, thisArg?: any): Collection<K, V>;
 		public find(fn: (value: V, key: K, collection: Collection<K, V>) => boolean): V;
 		public findKey(fn: (value: V, key: K, collection: Collection<K, V>) => boolean): K;
-		public first(): V;
+		public first(): V | undefined;
 		public first(count: number): V[];
-		public firstKey(): K;
+		public firstKey(): K | undefined;
 		public firstKey(count: number): K[];
 		public keyArray(): K[];
-		public last(): V;
+		public last(): V | undefined;
 		public last(count: number): V[];
-		public lastKey(): K;
+		public lastKey(): K | undefined;
 		public lastKey(count: number): K[];
 		public map<T>(fn: (value: V, key: K, collection: Collection<K, V>) => T, thisArg?: any): T[];
 		public partition(fn: (value: V, key: K, collection: Collection<K, V>) => boolean): [Collection<K, V>, Collection<K, V>]
-		public random(): V;
+		public random(): V | undefined;
 		public random(count: number): V[];
-		public randomKey(): K;
+		public randomKey(): K | undefined;
 		public randomKey(count: number): K[];
 		public reduce<T>(fn: (accumulator: any, value: V, key: K, collection: Collection<K, V>) => T, initialValue?: any): T;
 		public some(fn: (value: V, key: K, collection: Collection<K, V>) => boolean, thisArg?: any): boolean;
